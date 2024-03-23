2edd742c77b6c5936707835bfb02464620bb3c65
riteshreg <riteshregmi85@gmail.com>
2024-03-23 14:53:07 +0545

    remove_x

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const app = require('express')();
-
 const port = process.env.PORT ||  8080
-
+const {sum} = require('./sum_moudle/utils/sum')
 
 app.get('/', (req, res)=>{
     res.json({status:"ok"})
+})
+
+app.get('/y', (req, res)=>{
+    const result = sum(1,2);
+    res.json(result)
 })
 
 app.listen(port, ()=>{
